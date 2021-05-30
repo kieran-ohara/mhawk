f10b5773e82e4abc5cca901ebe94f3ede33dc01b
Kieran Bamforth <hello@kieranbamforth.me>
2021-05-31 00:39:13 +0100

    Add UI for viewing the monthly payments.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,53 @@
-import '../styles/globals.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Head from 'next/head';
+import React from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const classes = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      </Head>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            mhawk
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
