f4a5ef6d18a010938641992a45572b78b73ec35f
Kieran Bamforth <hello@kieranbamforth.me>
2021-05-30 21:26:12 +0100

    Make payment plans endpoint accept payment plans instead of totals.
    Move totals to /api/v0/payment-plans-with-total.

--- pages/api/v0/payment-plans-with-total.js
+++ pages/api/v0/payment-plans-with-total.js
@@ -1,11 +1,11 @@
 import debug from 'debug';
-import { addMonthlyPayments } from '../../../lib/payment-plans';
+import { addPaymentPlansUsingTotal } from '../../../lib/payment-plans';
 
 const log = debug('mhawk-monthly-payments');
 
 const postController = async function postController(req, res) {
   try {
-    const content = await addMonthlyPayments(req.body);
+    const content = await addPaymentPlansUsingTotal(req.body);
     res.status(200).json(content);
   } catch (error) {
     log(error);
