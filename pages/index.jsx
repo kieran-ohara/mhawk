b2363463160011475d501cb51b69e4dd8c870ba8
Kieran Bamforth <hello@kieranbamforth.me>
2021-05-31 12:09:20 +0100

    Make grid items more compact so they can be seen on the page.

--- pages/index.jsx
+++ pages/index.jsx
@@ -64,7 +64,7 @@ export default function Home() {
     <div style={{ height: 'calc(100% - 64px)' }}>
       <div style={{ display: 'flex', height: '100%' }}>
         <div style={{ flexGrow: 1 }}>
-          <DataGrid rows={data} columns={columns} pageSize={25} />
+          <DataGrid rows={data} columns={columns} pageSize={25} density="compact" />
         </div>
       </div>
     </div>
