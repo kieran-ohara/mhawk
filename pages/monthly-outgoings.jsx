9fe09351dc8b2360fabd5f17aa089be7cd2335ec
Kieran Bamforth <hello@kieranbamforth.me>
2021-06-01 01:39:35 +0100

    Create right drawer in Monthly Outgoings page that changes the month.

--- pages/monthly-outgoings.jsx
+++ pages/monthly-outgoings.jsx
@@ -1,3 +1,11 @@
+import Hidden from '@material-ui/core/Hidden';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import Link from 'next/link';
+
+import { makeStyles } from '@material-ui/core/styles';
 import { useRouter } from 'next/router';
 import { PaymentPlan } from '../components/payment-plan';
 
@@ -10,28 +18,91 @@ const columns = [
   },
 ];
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    height: '100%',
+  },
+  content: {
+    flexGrow: 1,
+  },
+  aside: {
+    width: drawerWidth,
+  },
+  calendar: {
+    display: 'flex',
+  },
+  calendarMonth: {
+    flexGrow: 1,
+    textAlign: 'center',
+    lineHeight: 3,
+  },
+}));
+
+function toYYYYMMDD(date) {
+  return date.toISOString().split('T')[0];
+}
+
 export default function Home() {
+  const classes = useStyles();
   const router = useRouter();
   // eslint-disable-next-line
   const { payments_for_month } = router.query;
 
-  let paymentsForMonth = new Date();
-  let dateString = paymentsForMonth.toISOString().split('T')[0];
-  // eslint-disable-next-line
-  if (payments_for_month) {
-    try {
-      paymentsForMonth = new Date(payments_for_month);
-      // eslint-disable-next-line
-      dateString = paymentsForMonth.toISOString().split('T')[0];
-    } catch (error) {
-      console.log(error);
-    }
+
+  // LOL
+  let paymentsForMonth;
+  let dateString;
+  let previousMonth;
+  let nextMonth;
+  try {
+    paymentsForMonth = new Date(payments_for_month);
+    dateString = toYYYYMMDD(paymentsForMonth);
+    previousMonth = new Date(paymentsForMonth.setMonth(paymentsForMonth.getMonth() - 1));
+    nextMonth = new Date(paymentsForMonth.setMonth(paymentsForMonth.getMonth() + 2));
+    paymentsForMonth.setMonth(paymentsForMonth.getMonth() - 1);
+  } catch (error) {
+    paymentsForMonth = new Date();
+    dateString = toYYYYMMDD(paymentsForMonth);
+    previousMonth = new Date(paymentsForMonth.setMonth(paymentsForMonth.getMonth() - 1));
+    nextMonth = new Date(paymentsForMonth.setMonth(paymentsForMonth.getMonth() + 2));
+    paymentsForMonth.setMonth(paymentsForMonth.getMonth() - 1);
   }
 
+  const options = { month: 'long' };
+  const dateLocaleString = paymentsForMonth.toLocaleString('en-GB', options);
+
   return (
-    <PaymentPlan
-      dataURI={`/api/v0/payment-plans?payments_for_month=${dateString}`}
-      columns={columns}
-    />
+    <>
+      <div className={classes.root}>
+        <div className={classes.content}>
+          <PaymentPlan
+            dataURI={`/api/v0/payment-plans?payments_for_month=${dateString}`}
+            columns={columns}
+          />
+        </div>
+        <Hidden xsDown>
+          <aside className={classes.aside}>
+            <div className={classes.calendar}>
+              <Link href={`?payments_for_month=${toYYYYMMDD(previousMonth)}`}>
+                <IconButton aria-label="delete">
+                  <NavigateBeforeIcon />
+                </IconButton>
+              </Link>
+              <Typography variant="subtitle1" noWrap className={classes.calendarMonth}>
+                {dateLocaleString}
+              </Typography>
+              <Link href={`?payments_for_month=${toYYYYMMDD(nextMonth)}`}>
+                <IconButton aria-label="delete">
+                  <NavigateNextIcon />
+                </IconButton>
+              </Link>
+            </div>
+          </aside>
+        </Hidden>
+      </div>
+    </>
   );
 }
