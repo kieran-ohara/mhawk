9303ad41870736af33c44504094d170d50609c0a
Kieran Bamforth <hello@kieranbamforth.me>
2021-06-04 22:59:03 +0100

    Fix the SQL file for using months instead of days.

--- sql/05-use-months-instead.sql
+++ sql/05-use-months-instead.sql
@@ -5,6 +5,6 @@ SET t1.monthly_price = t1.total_price / (PERIOD_DIFF(
     EXTRACT(YEAR_MONTH FROM t1.end_date),
     EXTRACT(YEAR_MONTH FROM t1.start_date)
     ) +1)
-WHERE t1.id = t2.id
+WHERE t1.id = t2.id;
 
 ALTER TABLE `mhawk`.`payment_plans` DROP COLUMN `total_price`;
