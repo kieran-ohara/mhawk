cf19e30328cdc9d891e251525ca833e79abfff26
Kieran Bamforth <hello@kieranbamforth.me>
2021-06-05 01:05:53 +0100

    Small refactor to setoff two promises instead of dual await.

--- pages/api/v0/monthly-outgoings.js
+++ pages/api/v0/monthly-outgoings.js
@@ -11,16 +11,19 @@ const getController = async function getController(req, res) {
       date = new Date(req.query.date);
     }
 
-    const result = await getPaymentPlansActiveForMonth(date);
-    const sumOutgoings = result.reduce((acc, item) => acc + item.monthly_price, 0);
-    const grossMonth = await getTaxedWage();
+    const result = await Promise.all([
+      getPaymentPlansActiveForMonth(date),
+      getTaxedWage(),
+    ]);
+    const [items, grossMonth] = result;
+    const sumOutgoings = (items.reduce((acc, item) => acc + item.monthly_price, 0)).toFixed(2);
 
     res.status(200).json({
       date,
       net_month: grossMonth - sumOutgoings,
       gross_month: grossMonth,
       sum: sumOutgoings,
-      items: result,
+      items,
     });
   } catch (error) {
     log(`error: ${error}`);
