7590613c862660ec1538a23a2a612c89f37c46df
Kieran Bamforth <hello@kieranbamforth.me>
2021-06-23 20:04:12 +0100

    Rename refactor payment-plan-mutations to payment-plans.

--- pages/payment-plans/finite.jsx
+++ pages/payment-plans/finite.jsx
@@ -7,7 +7,7 @@ import Fab from '@material-ui/core/Fab';
 import Tooltip from '@material-ui/core/Tooltip';
 
 import CreatePaymentPlanWithTotalDialog from '../../components/create-payment-plan-with-total';
-import usePaymentPlans from '../../hooks/payment-plan-mutations';
+import usePaymentPlans from '../../hooks/payment-plans';
 import { PaymentPlan, renderDate } from '../../components/payment-plan';
 
 import FabContainer from '../../components/fab-container';
