3705b16ffc9ebd93d731594816a5ccf09bcc1396
Kieran Bamforth <hello@kieranbamforth.me>
2021-06-26 19:19:04 +0100

    Create hook for interacting with tags.

---
+++ hooks/tags.js
@@ -0,0 +1,17 @@
+import useSWR from 'swr';
+
+export default function useTags() {
+  const { data, error } = useSWR(
+    '/api/v0/tags',
+    async (req) => {
+      const response = await fetch(req);
+      return response.json();
+    },
+  );
+
+  return {
+    tags: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
