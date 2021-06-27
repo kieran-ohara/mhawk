457beeb5c21388a06048234652602e21f95b2943
Kieran Bamforth <hello@kieranbamforth.me>
2021-06-27 13:41:47 +0100

    Allow user tag payment plan using REST API.

--- pages/api/v0/payment-plan/[id]/tags.js
+++ pages/api/v0/payment-plan/[id]/tags.js
@@ -1,5 +1,5 @@
 import debug from 'debug';
-import { getTagsForPaymentPlan } from '../../../../../lib/tags';
+import { getTagsForPaymentPlan, tagPaymentPlan } from '../../../../../lib/tags';
 
 const log = debug('mhawk-tags-by-id');
 
@@ -13,12 +13,24 @@ const getController = async function postController(req, res) {
   }
 };
 
+const postController = async function postController(req, res) {
+  try {
+    const result = await tagPaymentPlan(req.query.id, req.body.tag_id);
+    return res.status(200).send(result);
+  } catch (error) {
+    log(error);
+    return res.status(500).end();
+  }
+};
+
 export default function handler(req, res) {
   const { method } = req;
 
   switch (method) {
     case 'GET':
       return getController(req, res);
+    case 'POST':
+      return postController(req, res);
     default:
       return res.status(400);
   }
