6f56b7aa18dd21df83ac3dc9fe48ad50582dc241
Kieran Bamforth <hello@kieranbamforth.me>
2021-07-12 14:44:11 +0100

    Add the search page

--- components/app-frame.jsx
+++ components/app-frame.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
@@ -21,17 +22,19 @@ import DoneAllIcon from '@material-ui/icons/DoneAll';
 import EventIcon from '@material-ui/icons/Event';
 import MenuIcon from '@material-ui/icons/Menu';
 import ReplayIcon from '@material-ui/icons/Replay';
+import SearchIcon from '@material-ui/icons/Search';
 
 import Typography from '@material-ui/core/Typography';
+
+import InputBase from '@material-ui/core/InputBase';
 
 import {
   makeStyles,
   useTheme,
 } from '@material-ui/core/styles';
 
+import { signOut } from 'next-auth/client';
 import TagsLinks from './tags-links';
-
-import { signOut } from "next-auth/client"
 
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
@@ -67,6 +70,45 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    // backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      // backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
   },
 }));
 
@@ -123,6 +165,14 @@ function AppFrame(props) {
   const { window, title } = props;
   const container = window !== undefined ? () => window().document.body : undefined;
 
+  const router = useRouter();
+
+  const onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      router.push(`search?q=${event.target.value}`);
+    }
+  };
+
   return (
     <>
       <div className={classes.root}>
@@ -140,6 +190,20 @@ function AppFrame(props) {
             <Typography variant="h6" className={classes.title} noWrap>
               {title}
             </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+                onKeyPress={onKeyPress}
+              />
+            </div>
             <div>
               <IconButton
                 aria-label="account of current user"
