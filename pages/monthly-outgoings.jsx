8484907fa226ce58b228c1f3421f175a1bee987c
Kieran Bamforth <hello@kieranbamforth.me>
2021-07-18 23:43:31 +0100

    Fix linting.

--- pages/monthly-outgoings.jsx
+++ pages/monthly-outgoings.jsx
@@ -1,20 +1,14 @@
 import useSWR from 'swr';
 
-import Hidden from '@material-ui/core/Hidden';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-import Link from 'next/link';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
 import { makeStyles } from '@material-ui/core/styles';
 import { useRouter } from 'next/router';
+import Grid from '@material-ui/core/Grid';
 import { PaymentPlanGrid } from '../components/payment-plan';
 import usePaymentPlanMutations from '../hooks/payment-plans';
 import Chart from '../components/chart';
 
-import Grid from '@material-ui/core/Grid';
 import AppFrame from '../components/app-frame';
 
 const paymentHasEndDate = (params) => params.getValue(params.id, 'end_date') !== null;
@@ -42,9 +36,9 @@ const useStyles = makeStyles((theme) => ({
   dataGrid: {
     display: 'flex',
     height: `calc(100vh - ${appBarHeight + chartHeight}px)`,
-    [theme.breakpoints.up("lg")]: {
+    [theme.breakpoints.up('lg')]: {
       height: `calc(100vh - ${appBarHeight}px)`,
-    }
+    },
   },
   content: {
     flexGrow: 1,
@@ -75,24 +69,15 @@ export default function MonthlyOutgoings() {
   // LOL
   let paymentsForMonth;
   let dateString;
-  let previousMonth;
-  let nextMonth;
   try {
     paymentsForMonth = new Date(payments_for_month);
     dateString = toYYYYMMDD(paymentsForMonth);
-    previousMonth = new Date(paymentsForMonth.setMonth(paymentsForMonth.getMonth() - 1));
-    nextMonth = new Date(paymentsForMonth.setMonth(paymentsForMonth.getMonth() + 2));
     paymentsForMonth.setMonth(paymentsForMonth.getMonth() - 1);
   } catch (error) {
     paymentsForMonth = new Date();
     dateString = toYYYYMMDD(paymentsForMonth);
-    previousMonth = new Date(paymentsForMonth.setMonth(paymentsForMonth.getMonth() - 1));
-    nextMonth = new Date(paymentsForMonth.setMonth(paymentsForMonth.getMonth() + 2));
     paymentsForMonth.setMonth(paymentsForMonth.getMonth() - 1);
   }
-
-  const options = { month: 'long' };
-  const dateLocaleString = paymentsForMonth.toLocaleString('en-GB', options);
 
   const { getActiveMutationsForDate } = usePaymentPlanMutations();
 
