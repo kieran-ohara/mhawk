704adaca40c6fec02e5a63f1f0fac7d4d6184ad8
Kieran Bamforth <hello@kieranbamforth.me>
2022-07-22 22:15:35 +0100

    Hoist payment more menu into own component.

---
+++ components/payment-more-menu.tsx
@@ -0,0 +1,40 @@
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import LabelIcon from '@mui/icons-material/Label';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ListItemText from '@mui/material/ListItemText';
+
+export default function PaymentMoreMenu(props) {
+  const {
+    anchorEl,
+    open,
+    onClose,
+    onTagsClick,
+    onDeleteClick,
+  } = props;
+  return (
+    <Menu
+      id="basic-menu"
+      anchorEl={anchorEl}
+      open={open}
+      onClose={onClose}
+      MenuListProps={{
+        'aria-labelledby': 'basic-button',
+      }}
+    >
+      <MenuItem onClick={onTagsClick}>
+        <ListItemIcon>
+          <LabelIcon fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>Tags</ListItemText>
+      </MenuItem>
+      <MenuItem onClick={onDeleteClick}>
+        <ListItemIcon>
+          <DeleteIcon fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>Delete</ListItemText>
+      </MenuItem>
+    </Menu>
+  )
+}
