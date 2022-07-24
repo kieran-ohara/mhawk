92b91657380657de763cb84a81f57d5f0ac028f4
Kieran Bamforth <hello@kieranbamforth.me>
2022-07-24 15:59:30 +0100

    Fix types for subscriptions.ts.

--- pages/subscriptions.tsx
+++ pages/subscriptions.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, ChangeEvent, MouseEvent, } from "react";
 
 import AppFrame from "../components/app-frame";
 
@@ -7,9 +7,11 @@ import {
   PaymentsGrid,
 } from "../components/payments-grid";
 import PaymentMoreMenu from "../components/payment-more-menu";
-import { useSubscriptions, useSubscription } from "../hooks/subscriptions";
+import { useSubscriptions } from "../hooks/subscriptions";
+import { useOutgoing } from '../hooks/outgoings';
 import PaymentMoreIcon from "../components/payment-more-icon";
 import PaymentTags from "../containers/payment-tags";
+import { GridCellParams } from '@mui/x-data-grid';
 
 export default function Subscriptions() {
   const [menuAnchorEl, setMenuAnchorEl] = useState(null);
@@ -24,7 +26,7 @@ export default function Subscriptions() {
     addTag,
     removeTag,
     update: updateSubscription,
-  } = useSubscription(subscriptionId);
+  } = useOutgoing(subscriptionId);
 
   const handlePaymentTagsClick = () => {
     setMenuAnchorEl(null);
@@ -36,24 +38,20 @@ export default function Subscriptions() {
     setMenuAnchorEl(null);
   };
 
-  const handlePaymentCellClick = (event) => {
-    setSubscriptionId(event.row.id);
-  };
-
-  const handleTagChecked = (event) => {
+  const handleTagChecked = (event: ChangeEvent<HTMLInputElement>) => {
     const { checked, value: tagId } = event.target;
     if (checked) {
-      addTag(tagId);
+      addTag(parseInt(tagId));
     } else {
-      removeTag(tagId);
+      removeTag(parseInt(tagId));
     }
   };
 
-  const handleIsCellEditable = (params) => {
+  const handleIsCellEditable = (params: GridCellParams) => {
     return params.colDef.field === "monthly_price";
   };
 
-  const handleProcessRowUpdate = async (after, before) => {
+  const handleProcessRowUpdate = async (after: any) => {
     const { monthly_price } = after;
     await updateSubscription({
       monthly_price,
@@ -72,9 +70,9 @@ export default function Subscriptions() {
         width: 180,
       },
     ],
-    MoreComponent: (
-      <PaymentMoreIcon onClick={(event) => setMenuAnchorEl(event.target)} />
-    ),
+    MoreComponent:
+      <PaymentMoreIcon onClick={(event: MouseEvent<HTMLElement>) => setMenuAnchorEl(event.target)} />
+    ,
   });
 
   return (
@@ -87,7 +85,7 @@ export default function Subscriptions() {
           isCellEditable={handleIsCellEditable}
           processRowUpdate={handleProcessRowUpdate}
           onProcessRowUpdateError={handleProcessRowUpdateError}
-          onCellClick={(event) => setSubscriptionId(event.row.id)}
+          onCellClick={(event: GridCellParams) => setSubscriptionId(event.row.id)}
         />
         <PaymentMoreMenu
           anchorEl={menuAnchorEl}
